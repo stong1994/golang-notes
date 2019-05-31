2a192ad711fe42919780ae688a904b51223f6d71
st5983 <498327580@qq.com>
2019-05-31 14:44:59 +0800

    完成对sync下的waitgroup的练习

--- sync-practise/practise-waitgroup/main.go
+++ sync-practise/practise-waitgroup/main.go
@@ -1,1 +1,18 @@
-package practise_waitgroup
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	wg := &sync.WaitGroup{}
+	for i :=0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			fmt.Println("current index is ", i)
+		}(i)
+	}
+	wg.Wait()
+}
--- sync-practise/practise-waitgroup/practise-value/main.go
+++ sync-practise/practise-waitgroup/practise-value/main.go
@@ -1,1 +1,25 @@
-package practise_value
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+func main() {
+	v := atomic.Value{}
+	ch := make(chan struct{})
+
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			v.Store(i)
+			ch <- struct{}{}
+		}(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		if _, ok := <- ch; ok {
+			val := v.Load().(int)
+			fmt.Println("val is ", val)
+		}
+	}
+}
