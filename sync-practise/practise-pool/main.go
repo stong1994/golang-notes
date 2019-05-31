aba4dfc681f18fce8c87b5304109151af02cc5fa
st5983 <498327580@qq.com>
2019-05-31 14:44:23 +0800

    完成对sync下的pool的练习

--- sync-practise/practise-pool/main.go
+++ sync-practise/practise-pool/main.go
@@ -1,1 +1,26 @@
-package practise_pool
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	p := sync.Pool{New:random}
+	for i := 5; i < 10; i ++ {
+		p.Put(i)
+	}
+	for i := 0; i < 20; i ++ {
+		fmt.Println("random num is ", p.Get())
+	}
+}
+
+
+// 当p.Get()获取到的值为nil时，获取此数据
+func random() interface{} {
+	return rand.Intn(5)
+}
--- sync-practise/practise-pool/pool.go
+++ sync-practise/practise-pool/pool.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"sync"
+	"time"
+)
+
+// 官方例子
+var bufPool = sync.Pool{
+	New: func() interface{} {
+		return new(bytes.Buffer)
+	},
+}
+
+func timeNow() time.Time {
+	return time.Unix(1136214245, 0)
+}
+
+func Log(w io.Writer, key, val string) {
+	// 获取临时对象，没有的话会調用New()创建
+	b := bufPool.Get().(*bytes.Buffer)
+	b.Reset() // 重置
+	b.WriteString(timeNow().UTC().Format(time.RFC3339))
+	b.WriteByte(' ')
+	b.WriteString(key)
+	b.WriteByte('=')
+	b.WriteString(val)
+	w.Write(b.Bytes())
+	// 将临时对象放回到 Pool 中
+	bufPool.Put(b)
+}
+
+func main() {
+	Log(os.Stdout, "path", "/search?q=flowers")
+}
