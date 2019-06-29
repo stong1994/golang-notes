d2d50b52bd04aef681bc35033b8cca2bb304644b
stong1994 <st5983@outlook.com>
2019-06-29 10:43:10 +0700

    增加命令模式

---
+++ design_model/command_test.go
@@ -0,0 +1,24 @@
+package design_model
+
+import "testing"
+
+func TestCommand(t *testing.T) {
+	ctrl := &Control{}
+	lightOnCommand := new(LightOnCommand)
+	lightOffCommand := new(LightOffCommand)
+	videoOnCommand := new(VideoOnCommand)
+	videoOffCommand := new(VideoOffCommand)
+
+	// 开灯
+	ctrl.SetCommand(lightOnCommand)
+	ctrl.Execute()
+	// 关灯
+	ctrl.SetCommand(lightOffCommand)
+	ctrl.Execute()
+	// 开电视
+	ctrl.SetCommand(videoOnCommand)
+	ctrl.Execute()
+	// 关电视
+	ctrl.SetCommand(videoOffCommand)
+	ctrl.Execute()
+}
